60295be14e62757350486cb5bc671caa47e3bb16
JM0015 <jiminnn51@gmail.com>
2023-11-08 22:26:51 +0900

    챗봇 목록 UI 구현

---
+++ app/src/main/java/knu/dong/teamproject/SelectChatbotActivity.kt
@@ -0,0 +1,11 @@
+package knu.dong.teamproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectChatbotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_chatbot)
+    }
+}

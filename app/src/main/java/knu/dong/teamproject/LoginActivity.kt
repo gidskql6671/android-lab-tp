96f17a06055aea45a8ac5351c14e908a3c4c63cc
이재현 <fanta4715@naver.com>
2023-11-10 13:12:32 +0900

    feat: LoginActivity 기능 구현
    1. 로그인 버튼 클릭시 챗복 목록 이동
    2. 이메일과 비밀번호 검증 후 버튼 활성화
    3. 회원가입 페이지 이동

---
+++ app/src/main/java/knu/dong/teamproject/LoginActivity.kt
@@ -0,0 +1,56 @@
+package knu.dong.teamproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import knu.dong.teamproject.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            //로그인 로직 없이 바로 성공
+            val intent = Intent(this, SelectChatbotActivity::class.java)
+            startActivity(intent)
+        }
+
+        initTextWatcher()
+    }
+
+    private fun initTextWatcher() {
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                val email = binding.editEmail.text.toString()
+                val password = binding.editPassword.text.toString()
+                binding.btnLogin.isEnabled = validateEmail(email) && validatePassword(password)
+            }
+        }
+        binding.editEmail.addTextChangedListener(textWatcher)
+        binding.editPassword.addTextChangedListener(textWatcher)
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        return password.isNotBlank();
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return email.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+}

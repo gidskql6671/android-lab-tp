9c073595f37189f46123e5cd9255a161f408731b
gidskql6671 <gidskql6671@gmail.com>
2023-11-10 15:00:59 +0900

    리뷰 반영
    시간이 여유롭지 않아서 직접 수정함

--- app/src/main/java/knu/dong/teamproject/adapter/ChatbotListAdapter.kt
+++ app/src/main/java/knu/dong/teamproject/adapter/ChatbotListAdapter.kt
@@ -5,27 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import knu.dong.teamproject.databinding.ListviewItemChatBinding
 import knu.dong.teamproject.databinding.ListviewItemRoleBinding
 import knu.dong.teamproject.dto.Chatbot
 
-class ChatbotListAdapter(val context: Context, val chatList: MutableList<Chatbot>) : BaseAdapter() {
+class ChatbotListAdapter(val context: Context, val chatbotList: MutableList<Chatbot>) : BaseAdapter() {
     override fun getCount(): Int {
-        return chatList.size
+        return chatbotList.size
     }
 
     override fun getItem(position: Int): Any {
-        return chatList[position]
+        return chatbotList[position]
     }
 
-    override fun getItemId(position: Int): Long {
-        return 0
-    }
+    override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val binding = ListviewItemRoleBinding.inflate(LayoutInflater.from(context))
 
-        binding.itemId.text = chatList[position].name
+        binding.itemId.text = chatbotList[position].name
 
         return binding.root
     }

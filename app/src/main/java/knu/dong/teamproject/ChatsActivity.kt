5688947f72bb27386bd15041834651cb8d2ae3d3
gidskql6671 <gidskql6671@gmail.com>
2023-11-11 18:08:37 +0900

    실제 통신 없이 채팅 구현

--- app/src/main/java/knu/dong/teamproject/ChatsActivity.kt
+++ app/src/main/java/knu/dong/teamproject/ChatsActivity.kt
@@ -1,7 +1,9 @@
 package knu.dong.teamproject
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import knu.dong.teamproject.adapter.ChatsAdapter
@@ -12,6 +14,7 @@ import knu.dong.teamproject.dto.Chatbot
 
 class ChatsActivity: AppCompatActivity() {
     private lateinit var binding: ActivityChatsBinding
+    private lateinit var chatbot: Chatbot
     private val chats = mutableListOf<Chat>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +23,16 @@ class ChatsActivity: AppCompatActivity() {
         binding = ActivityChatsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val chatbot = intent.getSerializable("chatbot", Chatbot::class.java)
-        if (chatbot == null) {
-            finish()
-            return
+        chatbot = intent.getSerializable("chatbot", Chatbot::class.java)
+            ?: run {
+                finish()
+                return
+            }
+
+        binding.titleBar.btnBack.setOnClickListener{
+            onBackPressed()
         }
+        binding.titleBar.title.text = chatbot.name
 
         chats.addAll(0,
             listOf(
@@ -35,10 +43,26 @@ class ChatsActivity: AppCompatActivity() {
 
         initRecyclerView()
 
-        binding.titleBar.btnBack.setOnClickListener{
-            onBackPressed()
+        binding.btnSend.setOnClickListener {
+            val message = binding.editQuestion.text.toString()
+            if (message.isBlank()) {
+                return@setOnClickListener
+            }
+            binding.editQuestion.setText("")
+            val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.hideSoftInputFromWindow(
+                currentFocus!!.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
+
+
+            chats.add(Chat(message, true))
+            chats.add(Chat("$message 받았음", false))
+            binding.recyclerView.apply {
+                adapter?.notifyItemInserted(chats.size - 1)
+                smoothScrollToPosition(chats.size - 1)
+            }
         }
-        binding.titleBar.title.text = chatbot.name
     }
 
 

10be65d5c367e2be69a0c66852d432c05acc2f5d
chaejm55 <chaejm55@gmail.com>
2023-12-03 13:15:24 +0900

    [Fix] DTO 필드 오타 수정

--- app/src/main/java/knu/dong/teamproject/dto/Signup.kt
+++ app/src/main/java/knu/dong/teamproject/dto/Signup.kt
@@ -3,4 +3,4 @@ package knu.dong.teamproject.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Signup(val emailg: String, val verify: Int, val password:String)
+data class Signup(val email: String, val verify: Int, val password:String)

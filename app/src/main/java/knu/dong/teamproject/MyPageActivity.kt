753fd3150e4cb6afe8d15ad8e5e905cf1ad2db50
이재현 <fanta4715@naver.com>
2023-12-07 14:11:21 +0900

    feat: logout 구현

--- app/src/main/java/knu/dong/teamproject/MyPageActivity.kt
+++ app/src/main/java/knu/dong/teamproject/MyPageActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.edit
+import knu.dong.teamproject.common.HttpRequestHelper
 import knu.dong.teamproject.databinding.ActivityMyPageBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +19,26 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.titleBar.btnBack.setOnClickListener{
+        binding.titleBar.btnBack.setOnClickListener {
             onBackPressed()
         }
         binding.titleBar.title.text = "유저 페이지"
         binding.titleBar.btnAccount.visibility = View.INVISIBLE
+        binding.btnLogout.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                logout()
+            }
+        }
+    }
 
+    private suspend fun logout() {
+        HttpRequestHelper(this).post("api/users/logout")
+
+        getSharedPreferences("user_info", MODE_PRIVATE).edit {
+            remove("id")
+        }
+
+        val intent: Intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
